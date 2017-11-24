9a8833520fe87903a861d9f1056fb7cd24a79c90
Aleksandr Prianichnikov <pryaniq@gmail.com>
2017-11-24 22:02:42 +0200

    Create iOS project

---
+++ junk-robot64-ios/junk-robot64-iosTests/junk_robot64_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  junk_robot64_iosTests.swift
+//  junk-robot64-iosTests
+//
+//  Created by Aleksandr Prianichnikov on 24/11/2017.
+//  Copyright © 2017 robot64. All rights reserved.
+//
+
+import XCTest
+@testable import junk_robot64_ios
+
+class junk_robot64_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
