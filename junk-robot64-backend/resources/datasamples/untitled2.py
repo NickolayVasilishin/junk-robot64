9649630941f4718fe2f4643444b77fcc944380f8
Nikolay Vasilishin <nikolay_vasilishin@epam.com>
2017-11-25 12:37:15 +0100

    Added datasamples for #43 user

---
+++ junk-robot64-backend/resources/datasamples/untitled2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 12:29:35 2017
+
+@author: nikolay
+"""
+
+import pandas as pd
+
+medications_path = '/home/nikolay/study/junction/junk-robot64/junk-robot64-backend/resources/datasamples/medications_43'
+labs_path = '/home/nikolay/study/junction/junk-robot64/junk-robot64-backend/resources/datasamples/labs_43'
+
+#med = pd.read_csv(medications_path)
+lab = pd.read_csv(labs_path)
